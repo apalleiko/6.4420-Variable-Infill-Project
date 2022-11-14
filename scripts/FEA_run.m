a1e02b6fefd0a30a855c54b86e0b77315f640430
andrewj23 <apj23@mit.edu>
2022-11-13 21:27:43 -0500

    added visualization live script

--- scripts/FEA_run.m
+++ scripts/FEA_run.m
@@ -1,24 +1,28 @@
 clear all
 close all
+addpath '/Users/andrewjohnson/Desktop/MIT Academics/4th Year/6.4420/6.4420-Variable-Infill-Project/scripts'
 %%
 FEA = FEAfunctions;
 %%
 smodel = FEA.pdeModel('./cube.stl');
+figure(1)
+pdegplot(smodel,'VertexLabels','on','FaceLabels','on', 'FaceAlpha',.75)
 %%
 mesh = FEA.createMesh(smodel);
+figure(2)
+pdeplot3D(smodel)
 %%
 E = 4.107E9;
 nu = 0.3;
 fixedVertices = [];
-fixedFaces = [[3,5]];
+fixedFaces = [[1]];
 loadedFaces = [[2]];
-Faceforces = [[0;0;-1000]];
+Faceforces = [[0;0;-10]];
 loadedVertices = [[]];
 Vertexforces = [[]];
 [Rs, stressColors] = FEA.applyFEA(smodel,E,nu,fixedVertices,fixedFaces,loadedVertices,Vertexforces,loadedFaces,Faceforces);
-%%
-layerRange = [0 200];
-[layerX,layerY,layerStresses,Nb] = FEA.sliceLayer(smodel,mesh,Rs, stressColors, layerRange,true);
+figure(3)
+pdeplot3D(smodel,'ColorMapData',Rs.VonMisesStress,'Deformation',Rs.Displacement,'DeformationScaleFactor',100)
 %%
 layerHeight = 10;
-layers = FEA.generateAllLayers(smodel,mesh,Rs,stressColors,layerHeight,false)
+layers = FEA.generateAllLayers(smodel,mesh,Rs,stressColors,layerHeight,false);
